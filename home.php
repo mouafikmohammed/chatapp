5269feecc3b517b3875b8a3f94f59fb88af8a01e
mouafikmohammed <mouafikmohammed03@gmail.com>
2022-10-19 11:39:59 +0100

    last seen

--- home.php
+++ home.php
@@ -74,6 +74,23 @@
          </div>
       </div>
 
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+   <script>
+      $(document).ready(function(){
+         /**
+         auto update last seen
+         for logged in user
+         **/
+         let lastSeenUpdate = function() {
+            $.get("app/ajax/update_last_seen.php");
+         }
+         lastSeenUpdate();
+         /** 
+          auto update last seen every 10 sec
+         **/
+        setInterval(lastSeenUpdate, 10000);
+      });
+   </script>
 </body>
 </html>
 
